dd2e3749547ee2f6371e7b70652b9acdef64599c
Tim18769 <e282253@edu.sbl.ch>
2023-10-20 22:54:25 +0200

    Kommentare, Bilderzoom und ein Bild hinzugefügt

--- class.js
+++ class.js
@@ -1,10 +1,11 @@
+// Funktion, die prüft ob der Nutzername und das Passwort korrekt ist, falls ja wird der Text freigeschalten
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username === "Test" && password === "Hallo") {
+    if (username === "e282253" && password === "Password123") {  // <-- Nutzername und Passwort
         document.getElementById("loginBox").style.display = "none";
-        document.querySelector(".textManchesterUnited").style.filter = "none";
+        document.querySelector(".textManchesterUnited").style.filter = "none"; // <-- zum Aufheben des Blurs nachdem man sich eingeloggt hat
         document.querySelector(".textManchesterUnited").style.userSelect = "auto";
         document.querySelector(".textManchesterUnited").style.pointerEvents = "auto";
     } else {
